8a500f5a443ee96412741a776676043ea142e6e1
Sergio Sanchez <sergiosan2802@gmail.com>
2021-06-27 10:07:36 -0500

    feat implement routes and config init

--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Home from '../containers/home/Home'
 
 const App = () => {
-  return <h1>Init Route</h1>
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </Router>
+  )
 }
 
 export default App
